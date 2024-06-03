5629642c870b974b02b504891ad80337628158d3
Juan Martin <juanmartinsesali@gmail.com>
2024-06-03 05:41:26 -0300

    add: some kind of theme switcher

--- deps/magia.js
+++ deps/magia.js
@@ -18,5 +18,47 @@ window.$docsify.plugins = [].concat(function (hook, vm) {
 
       observer.observe(search, { attributes: true });
     }
+
+    // dark/light theme switcher
+    let themeSwitcher = document.createElement("button");
+    themeSwitcher.innerText = "Switch Theme";
+
+    console.log("darklight link", themeSwitcher);
+    const sidebar = document.getElementsByClassName("app-name")[0];
+    console.log("sidebar", sidebar);
+    sidebar.appendChild(themeSwitcher);
+
+    let darkThemeLink = document.querySelector('link[title="dark"]');
+    let lightThemeLink = document.querySelector('link[title="light"]');
+    let body = document.body;
+
+    if (!lightThemeLink.disabled) {
+      console.log("light mode");
+      body.classList.remove("dark-mode");
+    } else {
+      console.log("dark mode");
+      body.classList.add("dark-mode");
+    }
+
+    themeSwitcher.addEventListener("click", function () {
+      body.classList.add("transitioning");
+
+      if (darkThemeLink.disabled) {
+        console.log("light mode");
+        body.classList.remove("dark-mode");
+        // Current theme is light, switch to dark mode
+        darkThemeLink.disabled = false;
+        lightThemeLink.disabled = true;
+      } else {
+        console.log("dark mode");
+        body.classList.add("dark-mode");
+        // Current theme is dark, switch to light mode
+        darkThemeLink.disabled = true;
+        lightThemeLink.disabled = false;
+      }
+      setTimeout(function () {
+        body.classList.remove("transitioning");
+      }, 500); // Remove the transitioning class after 0.5s
+    });
   });
 }, window.$docsify.plugins);
