06a387cad7036f1fd5ad8b610e531f3bbb59dd1e
Juan Martin <juanmartinsesali@gmail.com>
2024-06-03 05:55:23 -0300

    fix: dark mode switcher and style with external lib

--- deps/magia.js
+++ deps/magia.js
@@ -19,46 +19,17 @@ window.$docsify.plugins = [].concat(function (hook, vm) {
       observer.observe(search, { attributes: true });
     }
 
-    // dark/light theme switcher
-    let themeSwitcher = document.createElement("button");
-    themeSwitcher.innerText = "Switch Theme";
-
-    console.log("darklight link", themeSwitcher);
+    // Agregar el botón de dark/light mode
+    const darkLightLink = document.querySelector("#docsify-darklight-theme");
+    console.log("darklight link", darkLightLink);
     const sidebar = document.getElementsByClassName("app-name")[0];
+    sidebar.appendChild(darkLightLink);
     console.log("sidebar", sidebar);
-    sidebar.appendChild(themeSwitcher);
 
-    let darkThemeLink = document.querySelector('link[title="dark"]');
-    let lightThemeLink = document.querySelector('link[title="light"]');
-    let body = document.body;
-
-    if (!lightThemeLink.disabled) {
-      console.log("light mode");
-      body.classList.remove("dark-mode");
-    } else {
-      console.log("dark mode");
-      body.classList.add("dark-mode");
-    }
-
-    themeSwitcher.addEventListener("click", function () {
-      body.classList.add("transitioning");
-
-      if (darkThemeLink.disabled) {
-        console.log("light mode");
-        body.classList.remove("dark-mode");
-        // Current theme is light, switch to dark mode
-        darkThemeLink.disabled = false;
-        lightThemeLink.disabled = true;
-      } else {
-        console.log("dark mode");
-        body.classList.add("dark-mode");
-        // Current theme is dark, switch to light mode
-        darkThemeLink.disabled = true;
-        lightThemeLink.disabled = false;
-      }
-      setTimeout(function () {
-        body.classList.remove("transitioning");
-      }, 500); // Remove the transitioning class after 0.5s
-    });
+    darkLightLink.style.position = "relative";
+    darkLightLink.style.display = "block";
+    darkLightLink.style.margin = "auto";
+    darkLightLink.style.top = "0";
+    darkLightLink.style.right = "0";
   });
 }, window.$docsify.plugins);
